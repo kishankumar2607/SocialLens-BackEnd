41a146260d18f3eec9e0bfc0d171f854ec9d336a
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-05-30 19:52:29 -0400

    added auth

---
+++ src/routes/authRoutes/authRoutes.js
@@ -0,0 +1,15 @@
+
+
+const express = require("express");
+const router = express.Router();
+const Auth = require("../../controllers/authController/AuthController");
+const protect = require("../../helper/authMiddleware");
+
+//Auth Routes
+router.post("/register", Auth.register);
+router.post("/login", Auth.login);
+router.post("/forgot-password", Auth.forgotPassword);
+router.post("/verify-otp", Auth.verifyOtp);
+router.post("/reset-password", Auth.resetPassword);
+
+module.exports = router;
