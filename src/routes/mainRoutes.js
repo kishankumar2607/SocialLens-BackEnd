ae3efee525c14d8aa69c1999cbf73c85482b82d3
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-05-30 20:28:45 -0400

    Update mainRoutes.js

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-// const Auth = require("./authRoutes/authRoutes");
+const Auth = require("./authRoutes/authRoutes");
 const Contact = require("./contactRoutes/contactRoutes");
 const Support = require("./supportRoutes/supportRoutes");
 
-// router.use("/auth", Auth);
+router.use("/auth", Auth);
 
 router.use("/api", Contact, Support);
 
