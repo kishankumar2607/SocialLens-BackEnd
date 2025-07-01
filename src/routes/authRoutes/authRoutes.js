1f0a37879debe0ef342822b8ac56685a8403ad6b
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-06-30 23:38:32 -0400

    Add update password route to authRoutes

--- src/routes/authRoutes/authRoutes.js
+++ src/routes/authRoutes/authRoutes.js
@@ -12,5 +12,6 @@ router.post("/verify-otp", Auth.verifyOtp);
 router.post("/reset-password", Auth.resetPassword);
 router.delete("/delete-account", protect, Auth.deleteAccount);
 router.put("/profile", protect, Auth.updateUserProfile);
+router.put("/update-password", protect, Auth.updatePassword);
 
 module.exports = router;
