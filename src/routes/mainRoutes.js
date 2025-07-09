ce40ff6fab8af8bff20e5c8334169af0ae4680e3
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-07-09 04:29:58 -0400

    Separate LinkedIn routes from auth routes

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -6,7 +6,9 @@ const Support = require("./supportRoutes/supportRoutes");
 const Account = require("./accountRoutes/accountRoutes");
 const LinkedIn = require("./linkedinRoutes/linkedinRoutes");
 
-router.use("/auth", Auth, LinkedIn);
+router.use("/auth", Auth);
+
+router.use("/auth/linkedin", LinkedIn);
 
 router.use("/api", Contact, Support, Account);
 
