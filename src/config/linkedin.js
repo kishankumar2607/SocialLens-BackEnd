bf9c38980fca318cbd67f379119a1efa1cdefeb1
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-07-09 04:29:02 -0400

    Add LinkedIn OAuth configuration module

---
+++ src/config/linkedin.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+module.exports = {
+  AUTH_URL: "https://www.linkedin.com/oauth/v2/authorization",
+  TOKEN_URL: "https://www.linkedin.com/oauth/v2/accessToken",
+  USERINFO_URL: "https://api.linkedin.com/v2/userinfo",
+  CLIENT_ID: process.env.LINKEDIN_CLIENT_ID,
+  CLIENT_SECRET: process.env.LINKEDIN_CLIENT_SECRET,
+  CALLBACK_URL: process.env.LINKEDIN_CALLBACK_URL,
+  SCOPES: ["openid", "profile", "email", "w_member_social"].join(" "),
+};
