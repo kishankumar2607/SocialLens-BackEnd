ef4cb4bf36fcf9688ad5b21c9d31d1089b8737b9
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-05 18:22:43 -0400

    Add generated post routes to main router

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -6,11 +6,12 @@ const Support = require("./supportRoutes/supportRoutes");
 const Account = require("./accountRoutes/accountRoutes");
 const LinkedIn = require("./linkedinRoutes/linkedinRoutes");
 const GeneratePost = require("./geminiRoutes/geminiRoutes");
+const GeneratedPostRoutes = require("./generatedPostRoutes/generatedPostRoutes");
 
 router.use("/auth", Auth);
 
 router.use("/auth/linkedin", LinkedIn);
 
-router.use("/api", Contact, Support, Account, GeneratePost);
+router.use("/api", Contact, Support, Account, GeneratePost, GeneratedPostRoutes);
 
 module.exports = router;
