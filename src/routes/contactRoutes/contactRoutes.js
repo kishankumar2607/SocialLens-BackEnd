63c38fc18a100296bdd8bf6223e984052249db27
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-15 20:07:35 -0400

    Update index.js

--- src/routes/contactRoutes/contactRoutes.js
+++ src/routes/contactRoutes/contactRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ContactController = require("../../controllers/contactController/contactController");
 
+// Contact Routes
 router.post("/contact-form", ContactController.postContact);
 router.get("/contacts", ContactController.getAllContact);
 router.delete("/contacts/:id", ContactController.deleteContactById);
